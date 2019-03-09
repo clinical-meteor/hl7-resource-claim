050e982ce550f17bba2d24af1ccf01f45027f2b2
Abigail Watson <awatson1978@gmail.com>
2019-03-08 20:49:00 -0600

    bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-claim',
-  version: '3.5.1',
+  version: '3.5.2',
   summary: 'HL7 FHIR Resource - Claim',
   git: 'https://github.com/clinical-meteor/hl7-resource-claim',
   documentation: 'README.md'
